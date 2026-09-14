5d88f0c52d302f53ec6b2ffcf450026128991784
Nathan Key <keyn@snookisd.org>
2026-09-14 15:24:02 -0500

    added tests

---
+++ bash-collection/dns-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FQDN="google.com"
+
+LOG_FILE="current_logs/dns_test_$(date +%F).log"
+exec >> "$LOG_FILE" 2>&1
+
+start_date=$(date +%F)
+while [ $(date +%F) = $start_date ]; do
+	if dig +short $FQDN > /dev/null 2>&1; then
+		echo "PASS time=$(date "+%F_%H-%M-%S")"
+	else
+		echo "FAIL time=$(date "+%F_%H-%M-%S")"
+	fi
+	sleep 1
+done
+
+mv $LOG_FILE old_logs
---
+++ bash-collection/uplink-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IP=8.8.8.8
+
+LOG_FILE="current_logs/uplink_ping_$(date "+%F").log"
+exec >> "$LOG_FILE" 2>&1
+
+start_date=$(date +%F)
+while [ "$(date +%F)" = "$start_date" ]; do
+	if ping -c 1 -W 1 "$IP" > /dev/null 2<&1; then
+		echo "PASS time=$(date "+%F_%H-%M-%S")"
+	else
+		echo "FAIL time=$(date "+%F_%H-%M-%S")"
+	fi
+	sleep 1
+done
+
+mv $LOG_FILE old_logs
